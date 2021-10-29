6f199d8f48e784323bbc8ddfccb52acadcc7497f
Michael Callaghan <mikecallaghan67@gmail.com>
2021-10-29 07:41:19 -0400

    Form validation (#20)
    * Re-enabled side-menu
    
    * Edit form with basic validation
    
    * Regex validation and better demo data
    
    * Show ion-items with and without a grid
    
    * Minor tweaks
    
    * ng 12.2.12

--- src/app/students.service.ts
+++ src/app/students.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 export interface Student {
   id: string;
@@ -13,16 +14,16 @@ export interface Student {
 }
 
 const mockStudents: Student[] = [
-  { id: '1', firstName: 'Greg', lastName: 'Marine' },
-  { id: '2', firstName: 'Jonathan', lastName: 'Bennett' },
-  { id: '3', firstName: 'Neil', lastName: 'Estandarte' },
-  { id: '4', firstName: 'Jennifer', lastName: 'Townsend' },
-  { id: '5', firstName: 'Casey', lastName: 'McBride' },
-  { id: '6', firstName: 'Diane', lastName: 'Rivera' },
-  { id: '7', firstName: 'Troy', lastName: 'Gutierrez' },
-  { id: '8', firstName: 'Priscilla', lastName: 'Little' },
-  { id: '9', firstName: 'Bobby', lastName: 'Robbins' },
-  { id: '10', firstName: 'Edmund', lastName: 'Gardner' }
+  { id: '1', firstName: 'Greg', lastName: 'Marine', birthDate: new Date('01/01/2000'), parentName: 'Bill Marine', parentEmail: 'billmarine@gmail.com', parentPhone: '555-555-5555' },
+  { id: '2', firstName: 'Jonathan', lastName: 'Bennett', birthDate: new Date('01/01/2000'), parentName: 'Bill Bennett', parentEmail: '', parentPhone: '555-555-5555' },
+  { id: '3', firstName: 'Neil', lastName: 'Estandarte', birthDate: new Date('01/01/2000'), parentName: 'Bill Estandarte', parentEmail: '', parentPhone: '555-555-5555' },
+  { id: '4', firstName: 'Jennifer', lastName: 'Townsend',  birthDate: new Date('01/01/2000'), parentName: 'Bill Townsend', parentEmail: '', parentPhone: '555-555-5555' },
+  { id: '5', firstName: 'Casey', lastName: 'McBride', birthDate: new Date('01/01/2000'), parentName: 'Bill McBride', parentEmail: '', parentPhone: '555-555-5555' },
+  { id: '6', firstName: 'Diane', lastName: 'Rivera', birthDate: new Date('01/01/2000'), parentName: 'Bill Rivera', parentEmail: '', parentPhone: '555-555-5555' },
+  { id: '7', firstName: 'Troy', lastName: 'Gutierrez', birthDate: new Date('01/01/2000'), parentName: 'Bill Gutierrez', parentEmail: '', parentPhone: '555-555-5555' },
+  { id: '8', firstName: 'Priscilla', lastName: 'Little', birthDate: new Date('01/01/2000'), parentName: 'Bill Little', parentEmail: '' },
+  { id: '9', firstName: 'Bobby', lastName: 'Robbins', birthDate: new Date('01/01/2000'), parentName: 'Bill Robbins', parentEmail: '', parentPhone: '555-555-5555' },
+  { id: '10', firstName: 'Edmund', lastName: 'Gardner', birthDate: new Date('01/01/2000'), parentName: 'Bill Gardner', parentEmail: '', parentPhone: '555-555-5555' },
 ];
 
 @Injectable({
@@ -35,6 +36,8 @@ export class StudentsService {
   getAll() {
     return [...mockStudents];
   }
+
+  getStudent(id: string): Observable<Student> {
+    return of(mockStudents.find(student => student.id === id));
+  }
 }
-
-
