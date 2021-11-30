08afc80c6af80f98629f81e2485d91301578ec58
Michael Callaghan <mikecallaghan67@gmail.com>
2021-11-30 17:10:43 -0500

    Add photo (#23)
    * Added capacitor and capacitor storage
    
    * Converted student service to promises?
    
    * Fixed some co-pilot issues
    
    * Fixed DeleteStudent, but not perfectly
    
    * Removed RxJS from storage service
    
    * Student CRUD working
    
    * Added some more functionality
    
    * Refactored some of the storage code
    
    * Added New Student functionality
    
    * Added camera feature
    
    * Standardize state management within the student service (#22)
    
    * refactor: add explicit modifiers to service signatures
    
    * refactor: simplify syntax in student service
    
    * fix: initialize service with stored data
    
    * refactor: move state changes into student service
    
    * refactor: use routerLinkActive to determine link state
    
    * chore: format all files with prettier
    
    * chore: prefer single quote for strings
    
    * Added camera feature
    
    * Made some minor tweeks for the workshop
    
    * Minor tweaks for conference
    
    Co-authored-by: benjcallaghan <benjcallaghan@users.noreply.github.com>

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -9,12 +9,7 @@ import { HomePageRoutingModule } from './home-routing.module';
 import { HomePage } from './home.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage]
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  declarations: [HomePage],
 })
 export class HomePageModule {}
--- src/app/roster/roster-routing.module.ts
+++ src/app/roster/roster-routing.module.ts
@@ -6,8 +6,8 @@ import { RosterPage } from './roster.page';
 const routes: Routes = [
   {
     path: '',
-    component: RosterPage
-  }
+    component: RosterPage,
+  },
 ];
 
 @NgModule({
